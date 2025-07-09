f3ed6eff41f5f8727c70978a9b6156fb8ff8e975
xyfuture <xyfuture@foxmail.com>
2025-07-09 14:29:30 +0800

    tmp

--- NeutronSim/Command/Commands.py
+++ NeutronSim/Command/Commands.py
@@ -1,21 +1,45 @@
-from dataclasses import dataclass
+from __future__ import annotations
+from dataclasses import dataclass, field
+from typing import Literal
 
 
 @dataclass
 class Shape:
-    pass
+    batch_size:int = -1
+    shape:list[int] = field(default= list)
+
+    chunk_num:int = -1
+    chunk_dim:int = -1
+
+    dtype:Literal['fp16','bf16','fp32'] = ''
+
+    @property
+    def chunk_size(self):
+        return  0
+
 
 
 @dataclass
 class CommandBase:
-    pass
+    opcode:str
+
+    prev_list:list[CommandBase]
+    next_list:list[CommandBase]
+
+
 
 
 
 @dataclass
 class SendCommand(CommandBase):
-    pass
 
+    # 地址协调的问题
+    src:int = -1
+    dst:int = -1
+    free:bool = False
+
+    dshape:Shape = field(default_factory=Shape)
+    
 
 @dataclass
 class ReceiveCommand(CommandBase):
